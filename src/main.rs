d9f2904b7b827fc3c20bf08d37af4f9c85597b65
MathisBurger <handymathis@gmail.com>
2021-03-03 19:40:07 +0100

    updated last files

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,16 @@ mod controller;
 mod mysql;
 mod models;
 
+// main function for webserver
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     HttpServer::new(|| {
         App::new()
             .wrap(Cors::new().supports_credentials().finish())
+            .service(controller::check_status_websocket::response)
             .route("/", web::get().to(controller::default_controller::response))
             .route("/activate-product", web::get().to(controller::activate_product_controller::response))
-            .service(controller::check_status_websocket::response)
             .route("/close-session", web::get().to(controller::close_session_controller::response))
             .route("/add-product", web::get().to(controller::add_product_controller::response))
             .route("/generate-product-key", web::get().to(controller::generate_key_controller::response))
--- src/mysql.rs
+++ src/mysql.rs
@@ -2,6 +2,7 @@ use sqlx::{MySqlPool, mysql};
 use std::io::Error;
 use crate::dotenv_handler;
 
+// creates connection
 pub async fn get_connection() -> Result<MySqlPool, Error> {
     Ok(mysql::MySqlPool::connect(&dotenv_handler::load_param("DATABASE_URL")).await.expect("Cannot connect to database"))
 }
